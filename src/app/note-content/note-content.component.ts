f442e2fc08e69da051a20e9198a1c88ee9d84b01
blubinskyj <blubinskyj@gmail.com>
2022-02-21 10:32:53 +0200

    update note

--- src/app/note-content/note-content.component.ts
+++ src/app/note-content/note-content.component.ts
@@ -1,14 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {StoreService} from "../shared/services/store.service";
 import {GroupsService} from "../shared/services/groups.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, fromEvent, Subscription} from "rxjs";
 import {Content, Group, Note} from "../shared/interfaces";
 import {NotesService} from "../shared/services/notes.service";
 import {ToastrService} from "ngx-toastr";
 import {debounceTime} from "rxjs/operators";
-
-
 
 
 @Component({
@@ -16,7 +14,12 @@ import {debounceTime} from "rxjs/operators";
   templateUrl: './note-content.component.html',
   styleUrls: ['./note-content.component.scss']
 })
-export class NoteContentComponent implements OnInit {
+export class NoteContentComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild('noteContent')
+  public noteContentInput?: ElementRef<HTMLTextAreaElement>
+
+  public textAreaSub?: Subscription
 
   public noteContent: Content = {
     content: ""
@@ -50,20 +53,11 @@ export class NoteContentComponent implements OnInit {
               private groupsService: GroupsService,
               private notesService: NotesService,
               private toastr: ToastrService) {
-    this.form.get("content")?.valueChanges.pipe(
-      debounceTime(3000)
-    ).subscribe(dataValue => {
-      this.noteContent.content = dataValue
-
-      this.notesService.updateNote(this.store.store.value.selectedGroupId,this.store.store.value.selectedNoteId, this.noteContent)
-        .subscribe(()=>{
-          this.fetchGroups()
-          this.toastr.success("Group was create")
-      })
-    })
   }
 
+
   ngOnInit(): void {
+
 
     this.store.store.subscribe((storeData) => {
       const targetGroup = storeData.groups.find((group) => {
@@ -75,20 +69,35 @@ export class NoteContentComponent implements OnInit {
         })
         if (targetNote) {
           this.note$.next(targetNote)
-        }else {
+        } else {
           this.note$.next(this.initialNote)
         }
       }
     })
   }
 
-  inputHendler(event: any){
-    const searchText = event.target.value
-    console.log(searchText)
-    // this.notesService.updateNote(this.store.store.value.selectedGroupId,this.store.store.value.selectedNoteId, this.noteContent)
+  ngAfterViewInit() {
+    if (this.noteContentInput)
+      this.textAreaSub = fromEvent(this.noteContentInput.nativeElement, 'input').pipe(
+        debounceTime(2000)
+      ).subscribe((value) => {
+        this.inputHandler(value)
+      })
   }
 
+  ngOnDestroy() {
+    this.textAreaSub?.unsubscribe()
+  }
 
+  inputHandler(event: any) {
+
+    this.noteContent.content =  event.target.value
+
+    this.notesService.updateNote(this.store.store.value.selectedGroupId,this.store.store.value.selectedNoteId, this.noteContent).subscribe(()=>{
+      this.fetchGroups()
+      this.toastr.success("Note was updated")
+    })
+  }
 
   onSubmit() {
   }
